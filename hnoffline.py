85a5c1a988d9b1b4476712ec995e2c9f93b30a15
Sachin Kelkar <sachinkel19@gmail.com>
2015-11-28 10:19:53 +0530

    Refactor and add Ask HN

--- hnoffline.py
+++ hnoffline.py
@@ -8,19 +8,43 @@ from jinja2 import FileSystemLoader, Environment
 # http://stackoverflow.com/a/19105436/4905313
 sys.version = '2.7.3 (default, Apr 12 2012, 14:30:37) [MSC v.1500 32 bit (Intel)]'
 
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-templateLoader = FileSystemLoader(THIS_DIR)
-templateEnv = Environment(loader=templateLoader, trim_blocks=True)
-TEMPLATE_FILE = "index.jinja2"
-template = templateEnv.get_template( TEMPLATE_FILE )
+def set_up_jinja2():
+    THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+    templateLoader = FileSystemLoader(THIS_DIR)
+    templateEnv = Environment(loader=templateLoader, trim_blocks=True)
+    TEMPLATE_FILE = "./templates/index.jinja2"
+    template = templateEnv.get_template( TEMPLATE_FILE )
+    return template
 
-hn = HackerNews()
+def get_top_stories(hn, limit=2):
+    """
+        Get the top 10 stories
+    """
+    top_stories = [hn.get_item(story_id) for story_id in hn.top_stories(limit=limit)]
+    return top_stories
 
-# Get the top 10 stories
-top_stories = [hn.get_item(story_id) for story_id in hn.top_stories(limit=5)]
-outputText = (template.render( top_stories = top_stories )).encode('utf-8')
+def get_ask_stories(hn, limit=2):
+    ask_stories = [hn.get_item(story_id) for story_id in hn.top_stories(limit=limit)]
+    return ask_stories
 
-# Make an output html file
-output = open("index.html", "w")
-output.write(outputText)
-output.close()
+def make_output_page(outputText, directory="", page_name="index"):
+    # Make an output html file
+    output = open(directory + page_name + ".html", "w")
+    output.write(outputText)
+    output.close()
+
+def render_template(stories, template):
+    """
+        Return the rendered template with the stories
+    """
+    return (template.render(stories=stories)).encode('utf-8')
+
+def main():
+    template = set_up_jinja2()
+    hn = HackerNews()
+    top_stories = get_top_stories(hn)
+    outputText = render_template(stories=top_stories, template=template)
+    make_output_page(outputText=outputText)
+
+if __name__ == '__main__':
+    main()
