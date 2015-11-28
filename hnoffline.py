f49d6c2444eea1dc60ecadb553ca21cf1407c599
Sachin Kelkar <sachinkel19@gmail.com>
2015-11-28 17:27:52 +0530

    Add navigation with Angular

--- hnoffline.py
+++ hnoffline.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
+import errno
+import shutil
 import sys
 
 from hackernews import HackerNews
@@ -18,32 +20,52 @@ def set_up_jinja2():
 
 def get_top_stories(hn, limit=2):
     """
-        Get the top 10 stories
+        Get the top stories
     """
     top_stories = [hn.get_item(story_id) for story_id in hn.top_stories(limit=limit)]
+    sys.stdout.write("Got the top stories\n")
     return top_stories
 
 def get_ask_stories(hn, limit=2):
-    ask_stories = [hn.get_item(story_id) for story_id in hn.top_stories(limit=limit)]
+    """
+        Get the Ask HN posts
+    """
+    ask_stories = [hn.get_item(story_id) for story_id in hn.ask_stories(limit=limit)]
+    sys.stdout.write("Got the ask stories\n")
     return ask_stories
 
-def make_output_page(outputText, directory="", page_name="index"):
+def make_output_page(outputText, directory="./output/", page_name="index"): # There's something wrong here :(
     # Make an output html file
     output = open(directory + page_name + ".html", "w")
     output.write(outputText)
     output.close()
 
-def render_template(stories, template):
+def render_template(templateVars, template):
     """
         Return the rendered template with the stories
     """
-    return (template.render(stories=stories)).encode('utf-8')
+    return (template.render(templateVars)).encode('utf-8')
+
+# http://stackoverflow.com/a/5032238/4905313
+def make_folder(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
 
 def main():
     template = set_up_jinja2()
     hn = HackerNews()
     top_stories = get_top_stories(hn)
-    outputText = render_template(stories=top_stories, template=template)
+    ask_stories = get_ask_stories(hn)
+    templateVars = {
+        "top_stories": top_stories,
+        "ask_stories": ask_stories
+    }
+    outputText = render_template(templateVars=templateVars, template=template)
+    make_folder("./output") # Make it take its value from a config file
+    shutil.copytree(src="./templates/js", dst="./output/js") # Error if folder already exists, need to workaround this
     make_output_page(outputText=outputText)
 
 if __name__ == '__main__':
